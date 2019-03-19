43e43855344743feb3d7da00dce1f7f894ebcb10
zhairui <zhairui@globmate.com>
2019-03-19 13:54:26 +0800

    新建工程

---
+++ UIButtonEdgeInsets/UIButtonEdgeInsets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIButtonEdgeInsets
+//
+//  Created by 全球友 on 2019/3/19.
+//  Copyright © 2019年 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIButtonEdgeInsets/UIButtonEdgeInsets/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIButtonEdgeInsets
+//
+//  Created by 全球友 on 2019/3/19.
+//  Copyright © 2019年 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
